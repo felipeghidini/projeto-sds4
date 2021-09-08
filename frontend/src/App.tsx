4f8102f6ee4cc6497e31a0afd241d6dd14e9e9d4
felipeghidini <lipeghidini18@gmail.com>
2021-09-07 22:36:42 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
+import NavBar from "components/NavBar";
+import DataTable from "components/NavBar/DataTable";
+import Footer from "components/NavBar/Footer";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Jesus é Booom</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Jesus é Booom</h1>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
